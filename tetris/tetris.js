80934a11340bb9912270f49181bf57d64ffb373e
baonguyen-stktutc2 <giabao.professional@gmail.com>
2024-05-23 14:38:24 +0700

    Update tetris.js

--- tetris/tetris.js
+++ tetris/tetris.js
@@ -236,7 +236,7 @@ const colors = [
     'purple',
 ];
 
-const arena = createMatrix(9, 16);
+const arena = createMatrix(18, 32);
 
 let nextPiece = createPiece('T');
 
@@ -250,8 +250,10 @@ const player = {
 document.addEventListener('keydown', event => {
     if (event.keyCode === 37) {
         playerMove(-1);
+        holdMove(-1);
     } else if (event.keyCode === 39) {
         playerMove(1);
+        holdMove(1);
     } else if (event.keyCode === 40) {
         playerDrop();
     } else if (event.keyCode === 81) {
